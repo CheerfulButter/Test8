ddfcaf2094e191a54b529438b05590cb644499a5
CheerfulButter <Brianshih380@GMAIL.COM>
2017-01-17 10:50:25 +0800

    Original/Main

--- app/src/main/java/cheerfulbutter/test8/Annoucement.java
+++ app/src/main/java/cheerfulbutter/test8/Annoucement.java
@@ -2,6 +2,7 @@ package cheerfulbutter.test8;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
 
 public class Annoucement extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class Annoucement extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_annoucement);
+
+        WebView view = new WebView(this);
+        view.getSettings().setJavaScriptEnabled(true);
+        view.loadUrl("file:///android_asset/HSdailyBulletin.html");
+        setContentView(view);
+
     }
 }
