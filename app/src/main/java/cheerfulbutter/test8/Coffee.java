83559a567851313a4341a552209f800a57003b51
CheerfulButter <Brianshih380@GMAIL.COM>
2017-02-08 09:51:21 +0800

    Removed action bar for Coffee order

--- app/src/main/java/cheerfulbutter/test8/Coffee.java
+++ app/src/main/java/cheerfulbutter/test8/Coffee.java
@@ -2,12 +2,14 @@ package cheerfulbutter.test8;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
 
 public class Coffee extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_coffee);
     }
 
